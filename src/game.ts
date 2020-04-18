1b217af45d0042495c83cc6d50022462840582a8
m.over <m.over@iplabs.de>
2020-04-18 13:00:37 +0200

    Game loop essentials

--- src/game.ts
+++ src/game.ts
@@ -1,17 +1,66 @@
-import { bar } from "./foo";
+
+interface GameObject {
+    draw(dt: number): void;
+    update(dt: number): void;
+}
+
+export class Game {
+
+    private canvas: HTMLCanvasElement;
+
+    private lastUpdateTime: number;
+
+    private dt: number = 0;
+
+    private boundLoop: () => void;
+
+    private gameObjects: GameObject[] = [];
+
+    constructor(canvas: HTMLCanvasElement) {
+        this.canvas = canvas;
+        this.lastUpdateTime = Date.now();
+        this.boundLoop = this.loop.bind(this);
+        this.loop();
+        this.gameObjects = [];
+    }
+
+    private loop() {
+        this.update();
+        this.draw();
+        requestAnimationFrame(this.boundLoop);
+    }
+
+    private update() {
+        const prevTime = this.lastUpdateTime;
+        this.lastUpdateTime = Date.now();
+        const dt = this.lastUpdateTime - prevTime;
+        this.dt = dt;
+        // Update all game classes
+        for (const obj of this.gameObjects) {
+            obj.update(dt);
+        }
+    }
+
+    private draw() {
+        const ctx = this.canvas.getContext("2d");
+        if (!ctx) {
+            return;
+        }
+
+        // Clear
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        ctx.fillStyle = "#" + Math.random().toString(16).substr(-6);
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        // Draw stuff
+        for (const obj of this.gameObjects) {
+            obj.draw(this.dt);
+        }
+    }
+
+
+}
 
 const canvas = document.querySelector<HTMLCanvasElement>("#gameCanvas")!;
-const ctx = canvas.getContext("2d")!;
-ctx.fillStyle = "#000";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-const img = new Image();
-img.onload = () => {
-    ctx.drawImage(img, (canvas.width - img.width) / 2, (canvas.height - img.height) / 2);
-    ctx.font = "20px sans-serif";
-    ctx.fillStyle = "white";
-    const metrics = ctx.measureText("Game not found");
-    ctx.fillText("Game not found", (canvas.width - metrics.width) / 2, canvas.height / 2 + img.height);
-};
-img.src = "assets/images/cross.png";
-
-bar();
+const game = new Game(canvas);
+(window as any).game = game;
