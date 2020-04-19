40da98f2b14bf9e8b8eb7d67fabacc666fa455a8
Ranjit Mevius <ranjit.mevius@outlook.com>
2020-04-19 16:03:23 +0200

    fix merge

--- src/ScriptedDialog.ts
+++ src/ScriptedDialog.ts
@@ -1,14 +1,5 @@
 import { NPC } from './NPC';
 import texts, { ScriptedDialogJSON } from "../assets/dummy.texts.json";
-/**
- * possible actions:
- * dialog _key_ - jump to npc dialog with key _key_
- * state add/remove _value_ - add/remove _value_ from campaignstate
- * face _mode_ - set the face mode
- * camera zoomin/zoomout
- * no action on an option will end the dialog
- */
-
 
 export class ScriptedDialog {
     public static Texts = texts;
