41f7b9c29602d62a061ab439f485e5192804254f
m.over <m.over@iplabs.de>
2020-04-21 01:40:36 +0200

    Don't shuffle conversation options

--- src/util.ts
+++ src/util.ts
@@ -72,3 +72,20 @@ export async function sleep(ms = 0): Promise<void> {
         setTimeout(() => resolve(), ms);
     });
 }
+
+/**
+ * Inplace array shuffling.
+ * @param array The array.
+ * @return The same array. But shuffled.
+ */
+export function shuffle<T>(array: T[]): T[] {
+    for (let i = 1; i < array.length; i++) {
+        const j = Math.floor(Math.random() * (i + 1));
+        if (i !== j) {
+            const tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+        }
+    }
+    return array;
+}
