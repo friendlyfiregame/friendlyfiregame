f4363e6d2e65e3e97838ba7d73f750d3c6aa03eb
Edu <eduke@gmx.de>
2020-04-20 20:10:21 +0200

    implement spawnWood on Tree

--- src/Tree.ts
+++ src/Tree.ts
@@ -7,17 +7,20 @@ import { TREE_ANIMATION } from "./constants";
 import { entity } from "./Entity";
 import { loadImage } from "./graphics";
 import { Seed } from "./Seed";
+import { Wood } from './Wood';
 
 @entity("tree")
 export class Tree extends NPC {
     private sprites!: Sprites;
     private spriteIndex = 0;
     public seed: Seed;
+    private wood: Wood;
 
     public constructor(game: Game, x: number, y:number) {
         super(game, x, y, 78, 140);
         this.face = new Face(this, EyeType.TREE, 1, 5, 94);
         this.seed = new Seed(game, x, y);
+        this.wood = new Wood(game, x, y);
         this.startDialog();
     }
 
@@ -51,5 +54,15 @@ export class Tree extends NPC {
         this.seed.y = this.y + this.height / 2;
         this.seed.setVelocity(5, 0);
         return this.seed;
+    }
+
+    public spawnWood(): Wood {
+        if (!this.game.gameObjects.includes(this.wood)) {
+            this.game.addGameObject(this.wood);
+        }
+        this.wood.x = this.x;
+        this.wood.y = this.y + this.height / 2;
+        this.wood.setVelocity(-5, 0);
+        return this.wood;
     }
 }
