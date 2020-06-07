a9a715de878c27eeb238fa2453737839b28b040e
Benjamin P. Jung <headcr4sh@gmail.com>
2020-06-07 12:25:48 +0200

    Put electron-forge config into extra file.
    Also: Enhance executable output with some richt metadata and
    add correct application icons.

---
+++ electron-forge.config.js
@@ -0,0 +1,62 @@
+const path = require("path")
+
+module.exports = {
+    "packagerConfig": {
+        name: "friendlyfire",
+        // https://electron.github.io/electron-packager/master/interfaces/electronpackager.win32metadataoptions.html
+        win32metadata: {
+            FileDescription: "A a small 2d platform adventure game with handcrafted pixel art, an original soundtrack and lots of love put into the creation of the characters and dialogs.",
+            ProductName: "Friendly Fire"
+        },
+        icon: path.resolve(__dirname, "assets", "appicon"),
+        appCopyright: "Copyright (C) 2020 Eduard But, Nico Huelscher, Benjamin Jung, Nils Kreutzer, Bastian Lang, Ranjit Mevius, Markus Over, " +
+        "Klaus Reimer and Jennifer van Veen",
+        appVersion: require(path.resolve(__dirname, "package.json")).version
+    },
+    "makers": [
+      {
+        "name": "@electron-forge/maker-squirrel",
+        "config": {
+          "name": "friendlyfire"
+        }
+      },
+      {
+        "name": "@electron-forge/maker-zip",
+        "platforms": [
+          "darwin"
+        ]
+      },
+      {
+        "name": "@electron-forge/maker-deb",
+        "config": {
+          "icon": "./assets/appicon/256x256.png",
+          "productName": "Friendly Fire",
+          "genericName": "Friendly Fire",
+          "categories": [
+            "Game"
+          ]
+        }
+      },
+      {
+        "name": "@electron-forge/maker-rpm",
+        "config": {}
+      }
+    ],
+    "plugins": [
+      [
+        "@electron-forge/plugin-webpack",
+        {
+          "mainConfig": "./webpack.main.config.js",
+          "renderer": {
+            "config": "./webpack.renderer.config.js",
+            "entryPoints": [
+              {
+                "js": "./lib/FriendlyFire.js",
+                "name": "./"
+              }
+            ]
+          }
+        }
+      ]
+    ]
+  }
