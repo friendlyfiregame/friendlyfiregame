724b7be059f5586216028e13abc138e7d147d4f2
m2u-84 <11140006+m2u-84@users.noreply.github.com>
2020-08-18 01:25:10 +0200

    Adjust character selection screen to geometry concepts

--- src/scenes/CharacterSelectionScene.ts
+++ src/scenes/CharacterSelectionScene.ts
@@ -1,18 +1,19 @@
-import { Scene } from "../Scene";
-import { FriendlyFire } from "../FriendlyFire";
-import { SlideTransition } from "../transitions/SlideTransition";
-import { easeOutCubic } from "../easings";
-import { BitmapFont } from "../BitmapFont";
-import { asset } from "../Assets";
-import { ControllerEvent } from "../input/ControllerEvent";
-import { DIALOG_FONT } from "../constants";
-import { ControllerManager } from '../input/ControllerManager';
-import { ControllerAnimationTags, ControllerSpriteMap } from '../input/ControllerFamily';
 import { Aseprite } from '../Aseprite';
-import { TitleScene } from './TitleScene';
-import { GameScene } from './GameScene';
-import { MenuList, MenuItem } from '../Menu';
+import { asset } from '../Assets';
+import { BitmapFont } from '../BitmapFont';
 import { CharacterAsset, VoiceAsset } from '../Campaign';
+import { ControllerAnimationTags, ControllerSpriteMap } from '../input/ControllerFamily';
+import { ControllerEvent } from '../input/ControllerEvent';
+import { ControllerManager } from '../input/ControllerManager';
+import { DIALOG_FONT } from '../constants';
+import { easeOutCubic } from '../easings';
+import { FriendlyFire } from '../FriendlyFire';
+import { GameScene } from './GameScene';
+import { MenuItem, MenuList } from '../Menu';
+import { Point, Size } from '../Geometry';
+import { Scene } from '../Scene';
+import { SlideTransition } from '../transitions/SlideTransition';
+import { TitleScene } from './TitleScene';
 
 enum MenuItemKey {
     CHARACTER = 'character',
@@ -21,9 +22,9 @@ enum MenuItemKey {
 }
 
 const menuItemX = 12;
-const characterMenuItemY = 20;
-const voiceMenuItemY = 50;
-const startMenuItemY = 124;
+const characterMenuItemPosition = new Point(menuItemX, 20);
+const voiceMenuItemPosition = new Point(menuItemX, 50);
+const startMenuItemPosition = new Point(menuItemX, 124);
 const selectionItemsYDistance = 12;
 const selectionItemsXDistance = 12
 
@@ -63,9 +64,9 @@ export class CharacterSelectionScene extends Scene<FriendlyFire> {
         this.outTransition = new SlideTransition({ duration: 0.25 });
 
         this.menu.setItems(
-            new MenuItem(MenuItemKey.CHARACTER, "Character:", CharacterSelectionScene.font, "black", menuItemX, characterMenuItemY),
-            new MenuItem(MenuItemKey.VOICE, "Voice:", CharacterSelectionScene.font, "black", menuItemX, voiceMenuItemY),
-            new MenuItem(MenuItemKey.START, "Start Game", CharacterSelectionScene.font, "black", menuItemX, startMenuItemY),
+            new MenuItem(MenuItemKey.CHARACTER, "Character:", CharacterSelectionScene.font, "black", characterMenuItemPosition),
+            new MenuItem(MenuItemKey.VOICE, "Voice:", CharacterSelectionScene.font, "black", voiceMenuItemPosition),
+            new MenuItem(MenuItemKey.START, "Start Game", CharacterSelectionScene.font, "black", startMenuItemPosition)
         )
     }
 
@@ -107,58 +108,58 @@ export class CharacterSelectionScene extends Scene<FriendlyFire> {
     }
 
 
-    private drawTooltip (ctx: CanvasRenderingContext2D, x: number, y: number, text: string, animationTag: ControllerAnimationTags) {
+    private drawTooltip (ctx: CanvasRenderingContext2D, position: Point, text: string, animationTag: ControllerAnimationTags) {
         const gap = 6;
-        const textPositionX = Math.round(x + this.controllerSpriteMapRecords[ControllerSpriteMap.KEYBOARD].width + gap);
-        const textPositionY = y;
+        const textPosition = position.clone().moveXBy(this.controllerSpriteMapRecords[ControllerSpriteMap.KEYBOARD].width + gap);
+
         const controllerSprite = ControllerManager.getInstance().controllerSprite;
-        this.controllerSpriteMapRecords[controllerSprite].drawTag(ctx, animationTag, x, y)
-        CharacterSelectionScene.font.drawTextWithOutline(ctx, text, textPositionX, textPositionY, "white", "black");
+        this.controllerSpriteMapRecords[controllerSprite].drawTag(ctx, animationTag, position)
+        CharacterSelectionScene.font.drawTextWithOutline(ctx, text, textPosition, "white", "black");
     }
 
-    public draw(ctx: CanvasRenderingContext2D, width: number, height: number) {
+    public draw(ctx: CanvasRenderingContext2D, size: Size) {
         ctx.save();
 
         ctx.globalAlpha = 0.8;
         ctx.fillStyle = "black";
-        ctx.fillRect(0, 0, width, height);
+        ctx.fillRect(0, 0, size.width, size.height);
 
         ctx.globalAlpha = 1;
 
-        const x = (width / 2) - CharacterSelectionScene.panelImage.width / 2;
-        const y = (height / 2) - (CharacterSelectionScene.panelImage.height / 2) - 16;
+        const x = (size.width / 2) - CharacterSelectionScene.panelImage.width / 2;
+        const y = (size.height / 2) - (CharacterSelectionScene.panelImage.height / 2) - 16;
         ctx.translate(x, y);
-        
+
         ctx.drawImage(CharacterSelectionScene.panelImage, 0, 0);
 
-        CharacterSelectionScene.headlineFont.drawText(ctx, "CHARACTER CUSTOMIZATION", 0, -14, "white");
-        this.drawTooltip(ctx, 0, CharacterSelectionScene.panelImage.height, "Select or Change", ControllerAnimationTags.CONFIRM);
-        this.drawTooltip(ctx, 0, CharacterSelectionScene.panelImage.height + 16, "Back", ControllerAnimationTags.BACK);
+        CharacterSelectionScene.headlineFont.drawText(ctx, "CHARACTER CUSTOMIZATION", new Point(0, -14), "white");
+        this.drawTooltip(ctx, new Point(0, CharacterSelectionScene.panelImage.height), "Select or Change", ControllerAnimationTags.CONFIRM);
+        this.drawTooltip(ctx, new Point(0, CharacterSelectionScene.panelImage.height + 16), "Back", ControllerAnimationTags.BACK);
 
         const character = this.game.campaign.selectedCharacter;
-        const charSelectionTextY = characterMenuItemY + selectionItemsYDistance;
-        const charSelectionTextX = menuItemX + selectionItemsXDistance;
+        const charSelectionTextPosition = characterMenuItemPosition.clone().moveBy(selectionItemsXDistance, selectionItemsYDistance);
         const charSelectenTextGap = 55;
+
         if (character === CharacterAsset.MALE) {
-            CharacterSelectionScene.font.drawText(ctx, "Variant 1", charSelectionTextX, charSelectionTextY, "grey");
-            CharacterSelectionScene.font.drawTextWithOutline(ctx, "Variant 2", charSelectionTextX + charSelectenTextGap, charSelectionTextY, "white", "black");
+            CharacterSelectionScene.font.drawText(ctx, "Variant 1", charSelectionTextPosition, "grey");
+            CharacterSelectionScene.font.drawTextWithOutline(ctx, "Variant 2", charSelectionTextPosition.clone().moveXBy(charSelectenTextGap), "white", "black");
         } else {
-            CharacterSelectionScene.font.drawTextWithOutline(ctx, "Variant 1", charSelectionTextX, charSelectionTextY, "white", "black");
-            CharacterSelectionScene.font.drawText(ctx, "Variant 2", charSelectionTextX + charSelectenTextGap, charSelectionTextY, "grey");
+            CharacterSelectionScene.font.drawTextWithOutline(ctx, "Variant 1", charSelectionTextPosition, "white", "black");
+            CharacterSelectionScene.font.drawText(ctx, "Variant 2", charSelectionTextPosition.clone().moveXBy(charSelectenTextGap), "grey");
         }
 
-        CharacterSelectionScene.playerSprites[character].drawTag(ctx, "idle", 213, 46);
+        CharacterSelectionScene.playerSprites[character].drawTag(ctx, "idle", new Point(213, 46));
 
         const voice = this.game.campaign.selectedVoice;
-        const voiceSelectionTextY = voiceMenuItemY + selectionItemsYDistance;
-        const voiceSelectionTextX = menuItemX + selectionItemsXDistance;
+        const voiceSelectionTextPosition = voiceMenuItemPosition.clone().moveBy(selectionItemsXDistance, selectionItemsYDistance);
         const voiceSelectenTextGap = 55;
+
         if (voice === VoiceAsset.MALE) {
-            CharacterSelectionScene.font.drawText(ctx, "High Pitch", voiceSelectionTextX, voiceSelectionTextY, "grey");
-            CharacterSelectionScene.font.drawTextWithOutline(ctx, "Low Pitch", voiceSelectionTextX + voiceSelectenTextGap, voiceSelectionTextY, "white", "black");
+            CharacterSelectionScene.font.drawText(ctx, "High Pitch", voiceSelectionTextPosition, "grey");
+            CharacterSelectionScene.font.drawTextWithOutline(ctx, "Low Pitch", voiceSelectionTextPosition.clone().moveXBy(voiceSelectenTextGap), "white", "black");
         } else {
-            CharacterSelectionScene.font.drawTextWithOutline(ctx, "High Pitch", voiceSelectionTextX, voiceSelectionTextY, "white", "black");
-            CharacterSelectionScene.font.drawText(ctx, "Low Pitch", voiceSelectionTextX + voiceSelectenTextGap, voiceSelectionTextY, "grey");
+            CharacterSelectionScene.font.drawTextWithOutline(ctx, "High Pitch", voiceSelectionTextPosition, "white", "black");
+            CharacterSelectionScene.font.drawText(ctx, "Low Pitch", voiceSelectionTextPosition.clone().moveXBy(voiceSelectenTextGap), "grey");
         }
 
         ctx.font = "20px sans-serif";
