abad6a6e9daa6aa8868217ab5b7d6d4c41c8ce95
Klaus Reimer <k@ailis.de>
2020-09-05 17:52:56 +0200

    Use scene graphi in loading scene

--- src/scenes/LoadingScene.ts
+++ src/scenes/LoadingScene.ts
@@ -1,10 +1,21 @@
 import { FriendlyFire } from "../FriendlyFire";
 import { Scene } from "../Scene";
 import { TitleScene } from "./TitleScene";
+import { ProgressBarNode } from "../scene/ProgressBarNode";
 
 export class LoadingScene extends Scene<FriendlyFire> {
-    private total = 100;
-    private loaded = 50;
+    private progressBar!: ProgressBarNode;
+
+    public setup(): void {
+        this.progressBar = new ProgressBarNode({
+            x: this.game.width >> 1,
+            y: this.game.height >> 1
+        }).appendTo(this.rootNode);
+    }
+
+    public cleanup(): void {
+        this.rootNode.clear();
+    }
 
     public async activate(): Promise<void> {
         await this.game.assets.load(this.updateProgress.bind(this));
@@ -12,33 +23,10 @@ export class LoadingScene extends Scene<FriendlyFire> {
     }
 
     private updateProgress(total: number, loaded: number): void {
-        this.total = total;
-        this.loaded = loaded;
-    }
-
-    public draw(ctx: CanvasRenderingContext2D, width: number, height: number): void {
-        if (this.loaded !== this.total) {
-            const progressWidth = 200;
-            const progressHeight = 8;
-            ctx.save();
-            ctx.strokeStyle = "#888";
-            ctx.fillStyle = "#222";
-
-            ctx.fillRect(
-                ((width - progressWidth) >> 1),
-                ((height - progressHeight) >> 1),
-                Math.round(progressWidth * this.loaded / this.total),
-                progressHeight
-            );
-
-            ctx.strokeRect(
-                ((width - progressWidth) >> 1) + 0.5,
-                ((height - progressHeight) >> 1) + 0.5,
-                progressWidth,
-                progressHeight
-            );
-
-            ctx.restore();
+        if (loaded < total) {
+            this.progressBar.setProgress(loaded / total);
+        } else {
+            this.progressBar.remove();
         }
     }
 }
