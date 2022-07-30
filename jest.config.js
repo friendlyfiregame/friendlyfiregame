42866389460d53c592660d1ed23d8d163429cc2f
Benjamin P. Jung <headcr4sh@gmail.com>
2022-07-30 13:14:44 +0200

    Get rid of jest<>sonar integration.

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
 // cSpell:disable
 // @ts-check
 
-const sonarScannerEnabled = (!!process.env.SONAR_SCANNER_ENABLED && process.env.SONAR_SCANNER_ENABLED !== "false") || false;
-
 /** @type {import("jest").Config} */
 const config = {
   "projects": [
@@ -12,9 +10,7 @@ const config = {
       "testEnvironment": "@jest-runner/electron/environment",
       "testMatch": [
         "<rootDir>/lib/test/**/*.test.js"
-      ],
-      "sdfsdf": "sdfsdf",
-      //"testResultsProcessor": sonarScannerEnabled ? "jest-sonar-reporter" : null
+      ]
     }
   ],
   "collectCoverageFrom": [
