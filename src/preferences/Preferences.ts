571371b7c83389513bc1a718607dd9926a28681b
Benjamin P. Jung <headcr4sh@gmail.com>
2022-09-12 20:48:26 +0200

    [WiP] Sound manager.

--- src/preferences/Preferences.ts
+++ src/preferences/Preferences.ts
@@ -1,17 +1,29 @@
 import { getCanvas } from "../graphics";
+import { getSfxGainNode, getMusicGainNode } from "../Sound";
 
 export interface Preferences {
     readonly fullscreen: {
+        enabled: boolean;
         setEnabled(enabled: boolean): Promise<void>;
         isEnabled(): Promise<boolean>;
     };
+    readonly audio: {
+        getMusicGain(): Promise<number>;
+        setMusicGain(volume: number): Promise<void>;
+        getSfxGain(): Promise<number>;
+        setSfxGain(volume: number): Promise<void>;
+    };
 }
+
+
 
 /**
  * Default preferences as backed by the browser-only version of the game.
  */
 const webPreferences: Preferences = {
     fullscreen: {
+        get enabled(): boolean { return (localStorage.getItem("fullscreen.enabled") || "true") === "true"; },
+        set enabled(enabled: boolean) { localStorage.setItem("fullscreen.enabled", enabled.toString()); },
         isEnabled: async () => Promise.resolve(document.fullscreenEnabled && document.fullscreenElement != null),
         setEnabled: async (fullscreen: boolean) => {
             if (fullscreen && document.fullscreenElement == null) {
@@ -23,6 +35,12 @@ const webPreferences: Preferences = {
             }
 
         }
+    },
+    audio: {
+        getMusicGain: async () => Promise.resolve(getMusicGainNode().gain.value),
+        setMusicGain: async(value: number) => { (getMusicGainNode().gain.value = value); },
+        getSfxGain: async () => Promise.resolve(getSfxGainNode().gain.value),
+        setSfxGain: async(value: number) => { (getSfxGainNode().gain.value = value); }
     }
 };
 
