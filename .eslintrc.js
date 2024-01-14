e121b93aa19354b867ab968674cbac466e66d208
Klaus Reimer <k@ailis.de>
2024-01-14 22:54:54 +0100

    Configure @typescript-eslint/brace-style eslint rule from error to warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,7 +41,13 @@ const config = {
                 }
             }
         ],
-        "@typescript-eslint/brace-style": [ "error", "1tbs", { "allowSingleLine": true } ],
+         "@typescript-eslint/brace-style": [
+            "warn",
+            "1tbs",
+            {
+                "allowSingleLine": true
+            }
+        ],
         "@typescript-eslint/comma-spacing": "warn",
         "@typescript-eslint/default-param-last": "warn",
         "@typescript-eslint/explicit-function-return-type": [
