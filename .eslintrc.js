639bca4f3cbca3903c95aa6bc3db765f8ad63d6d
Klaus Reimer <k@ailis.de>
2024-01-14 21:41:17 +0100

    Enable explicit-module-boundary-types eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,7 +39,7 @@ const config = {
                 "allowTypedFunctionExpressions": true
             }
         ],
-        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "warn",
         "@typescript-eslint/no-empty-function": "off",
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-floating-promises": [
--- src/web/Particles.ts
+++ src/web/Particles.ts
@@ -310,8 +310,8 @@ function trapezeFunction(v: number, v1: number = v): ((p: number) => number) {
 export const valueCurves = {
     constant: new ValueCurve((p) => 1, 1), // always 1
     linear: new ValueCurve((p) => p), // linear 0 to 1
-    trapeze: (v: number = 0.1, v1: number = v) => new ValueCurve(trapezeFunction(v, v1)), // blocky 0 to 1 to 0
-    cos: (v: number = 0.1, v1: number = v) => // smooth 0 to 1 to 0
+    trapeze: (v: number = 0.1, v1: number = v): ValueCurve => new ValueCurve(trapezeFunction(v, v1)), // blocky 0 to 1 to 0
+    cos: (v: number = 0.1, v1: number = v): ValueCurve => // smooth 0 to 1 to 0
             new ValueCurve((p) => 0.5 - 0.5 * Math.cos(Math.PI * trapezeFunction(v, v1)(p))),
     cubic: new ValueCurve((p) => 3 * p * p - 2 * p * p * p) // smooth 0 to 1
 };
