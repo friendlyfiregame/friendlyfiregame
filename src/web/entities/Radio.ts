16dda460aee57cc4e5544c1fa66625cca4aca728
Klaus Reimer <k@ailis.de>
2024-02-25 12:18:06 +0100

    Move Radio rendering to AsepriteNode

--- src/web/entities/Radio.ts
+++ src/web/entities/Radio.ts
@@ -1,7 +1,9 @@
 import { type Aseprite } from "../Aseprite";
 import { asset } from "../Assets";
 import { Entity, entity, type EntityArgs } from "../Entity";
+import { Direction } from "../geom/Direction";
 import { RenderingLayer } from "../Renderer";
+import { AsepriteNode } from "../scene/AsepriteNode";
 
 @entity("Radio")
 export class Radio extends Entity {
@@ -10,11 +12,11 @@ export class Radio extends Entity {
 
     public constructor(args: EntityArgs) {
         super({ width: 24, height: 24, ...args, isTrigger: false });
+        this.appendChild(new AsepriteNode({
+            aseprite: Radio.sprite,
+            tag: "idle",
+            layer: RenderingLayer.ENTITIES,
+            anchor: Direction.BOTTOM
+        }));
     }
-
-    public override render(): void {
-        this.scene.renderer.addAseprite(Radio.sprite, "idle", this.x, this.y, RenderingLayer.ENTITIES);
-    }
-
-    public override update(): void {}
 }
