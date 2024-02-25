eb0ee8d56b2d437eec512f31a3eebe254080f8b2
Klaus Reimer <k@ailis.de>
2024-02-25 12:21:34 +0100

    Move Chicken rendering to AsepriteNode

--- src/web/entities/Chicken.ts
+++ src/web/entities/Chicken.ts
@@ -3,7 +3,9 @@ import { type Aseprite } from "../Aseprite";
 import { asset } from "../Assets";
 import { Conversation } from "../Conversation";
 import { entity, type EntityArgs } from "../Entity";
+import { Direction } from "../geom/Direction";
 import { RenderingLayer } from "../Renderer";
+import { AsepriteNode } from "../scene/AsepriteNode";
 import { NPC } from "./NPC";
 
 @entity("Chicken")
@@ -15,6 +17,12 @@ export class Chicken extends NPC {
         super({ ...args, width: 24, height: 18 });
         this.lookAtPlayer = false;
         this.conversation = new Conversation(conversation, this);
+        this.appendChild(new AsepriteNode({
+            aseprite: Chicken.sprite,
+            tag: "idle",
+            layer: RenderingLayer.ENTITIES,
+            anchor: Direction.BOTTOM
+        }));
     }
 
     public override getInteractionText(): string {
@@ -26,13 +34,6 @@ export class Chicken extends NPC {
     }
 
     public override render(): void {
-        this.scene.renderer.addAseprite(
-            Chicken.sprite,
-            "idle",
-            this.x, this.y,
-            RenderingLayer.ENTITIES,
-            this.direction
-        );
         this.speechBubble.draw();
     }
 
