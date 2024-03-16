cc318aca95f52c71629feb3cc85b83cae534f6eb
Klaus Reimer <k@ailis.de>
2024-03-16 13:50:17 +0100

    Move Super Throw rendering to AsepriteNode

--- src/web/entities/SuperThrow.ts
+++ src/web/entities/SuperThrow.ts
@@ -3,7 +3,9 @@ import { type Aseprite } from "../Aseprite";
 import { asset } from "../Assets";
 import { Conversation } from "../Conversation";
 import { entity, type EntityArgs } from "../Entity";
+import { Direction } from "../geom/Direction";
 import { RenderingLayer } from "../Renderer";
+import { AsepriteNode } from "../scene/AsepriteNode";
 import { NPC } from "./NPC";
 
 @entity("SuperThrow")
@@ -12,11 +14,18 @@ export class SuperThrow extends NPC {
     private static readonly sprite: Aseprite;
     private readonly floatAmount = 4;
     private readonly floatSpeed = 2;
+    private readonly asepriteNode: AsepriteNode;
 
     public constructor(args: EntityArgs) {
         super({ ...args, width: 18, height: 22 });
         this.lookAtPlayer = false;
         this.conversation = new Conversation(conversation, this);
+        this.asepriteNode = new AsepriteNode({
+            aseprite: SuperThrow.sprite,
+            tag: "idle",
+            layer: RenderingLayer.ENTITIES,
+            anchor: Direction.BOTTOM
+        }).appendTo(this);
     }
 
     public override getInteractionText(): string {
@@ -28,22 +37,12 @@ export class SuperThrow extends NPC {
     }
 
     public override render(): void {
-        const floatOffsetY = Math.sin(this.timeAlive * this.floatSpeed) * this.floatAmount;
-
-        this.scene.renderer.addAseprite(
-            SuperThrow.sprite,
-            "idle",
-            this.x, this.y + floatOffsetY,
-            RenderingLayer.ENTITIES,
-            this.direction
-        );
-
         this.speechBubble.draw();
     }
 
     public override update(dt: number): void {
         super.update(dt);
-
+        this.asepriteNode.setY(Math.sin(this.timeAlive * this.floatSpeed) * this.floatAmount + 1);
         this.speechBubble.update(this.x, this.y);
     }
 }
