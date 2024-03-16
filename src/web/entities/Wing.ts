c1093f8990d23a4fdcb98709c0f4b13c965ae67b
Klaus Reimer <k@ailis.de>
2024-03-16 13:52:00 +0100

    Move Wing rendering to AsepriteNode

--- src/web/entities/Wing.ts
+++ src/web/entities/Wing.ts
@@ -1,20 +1,29 @@
 import { type Aseprite } from "../Aseprite";
 import { asset } from "../Assets";
 import { entity, type EntityArgs } from "../Entity";
+import { Direction } from "../geom/Direction";
 import { QuestATrigger, QuestKey } from "../Quests";
 import { RenderingLayer } from "../Renderer";
+import { AsepriteNode } from "../scene/AsepriteNode";
 import { NPC } from "./NPC";
 
 @entity("Wing")
 export class Wing extends NPC {
     @asset("sprites/wing.aseprite.json")
     private static readonly sprite: Aseprite;
-
     private readonly floatAmount = 4;
     private readonly floatSpeed = 2;
+    private readonly asepriteNode: AsepriteNode;
 
     public constructor(args: EntityArgs) {
         super({ ...args, width: 24, height: 24 });
+        this.asepriteNode = new AsepriteNode({
+            aseprite: Wing.sprite,
+            tag: "idle",
+            layer: RenderingLayer.ENTITIES,
+            anchor: Direction.BOTTOM
+        }).appendTo(this);
+
     }
 
     protected override showDialoguePrompt(): boolean {
@@ -29,15 +38,6 @@ export class Wing extends NPC {
     }
 
     public override render(): void {
-        const floatOffsetY = Math.sin(this.timeAlive * this.floatSpeed) * this.floatAmount;
-
-        this.scene.renderer.addAseprite(
-            Wing.sprite,
-            "idle",
-            this.x, this.y + floatOffsetY,
-            RenderingLayer.ENTITIES
-        );
-
         if (this.showDialoguePrompt()) {
             this.drawDialoguePrompt();
         }
@@ -47,7 +47,7 @@ export class Wing extends NPC {
 
     public override update(dt: number): void {
         super.update(dt);
-
+        this.asepriteNode.setY(Math.sin(this.timeAlive * this.floatSpeed) * this.floatAmount + 1);
         this.dialoguePrompt.update(dt, this.x, this.y - 16);
         this.speechBubble.update(this.x, this.y);
     }
